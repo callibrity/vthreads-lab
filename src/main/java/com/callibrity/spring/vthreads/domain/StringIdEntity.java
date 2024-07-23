bc2567c0542b50edf77d7f3dc5310b8029ab8dbf
James Carman <jcarman@callibrity.com>
2024-07-23 00:18:19 -0400

    Removing audit fields.

--- src/main/java/com/callibrity/spring/vthreads/domain/StringIdEntity.java
+++ src/main/java/com/callibrity/spring/vthreads/domain/StringIdEntity.java
@@ -1,23 +1,14 @@
 package com.callibrity.spring.vthreads.domain;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
 import lombok.Getter;
 
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.Instant;
 import java.util.UUID;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class StringIdEntity {
 
@@ -28,18 +19,6 @@ public abstract class StringIdEntity {
     @Version
     @Column(name = "version", nullable = false)
     private Long version;
-
-    @CreatedDate
-    private Instant createdDate;
-
-    @LastModifiedDate
-    private Instant modifiedDate;
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedBy
-    private String modifiedBy;
 
     protected StringIdEntity(String id) {
         this.id = id;
